dd62a4e6834622b105397387ea7abbfe0e53bcb8
Zainab Zulfiqar <shahzainab952@gmail.com>
2021-02-17 15:06:04 +0500

    addded my appp

---
+++ main.js
@@ -0,0 +1,12 @@
+var app=new Vue({
+
+    el:"#app",
+    data:{
+        product:"Jackets",
+        image:"./images/logo-image",
+        altText:"this is VueJs logo",
+        link:"https://www.google.com/search?q=smart+full+form&oq=smart+full+form&aqs=chrome..69i57j0l7.4354j0j7&sourceid=chrome&ie=UTF-8"
+    }
+
+
+})
